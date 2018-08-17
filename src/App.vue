a683f02cd4dc9524eae09ddee7ac82bcbccfb188
dcZhangTW <dczhang@thoughtworks.com>
2018-08-17 16:08:04 +0800

    [Init]

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="full-width center-content">
+    <hello-component name="World" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+    h1 {
+        color: #ebebeb;
+    }
+</style>
+
+<script>
+import HelloComponent from './components/HelloComponent.vue'
+export default {
+  components: {
+    HelloComponent
+  }
+}
+</script>
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import './assets/base.scss'
+
+/* eslint-disable-next-line no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
